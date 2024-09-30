b32d4fa312dd66d367978b1b1db99e448701c629
Aleksandr Soloshenko <capcom2me@gmail.com>
2024-09-30 07:00:33 +0700

    [feature] prepare deletion of over-limit backups

--- internal/config/config.go
+++ internal/config/config.go
@@ -17,18 +17,38 @@ type Storage struct {
 	URL string `envconfig:"STORAGE__URL"`
 }
 
+type BackupLimits struct {
+	MaxCount int `envconfig:"BACKUP__LIMITS__MAX_COUNT"`
+}
+
+type Backup struct {
+	Limits BackupLimits
+}
+
 type Config struct {
 	MariaDB MariaDB
 	Storage Storage
+	Backup  Backup
 }
 
 var onceLoader sync.Once
-var config Config
+var config = Config{
+	MariaDB: MariaDB{
+		User:     "root",
+		Password: "",
+	},
+	Storage: Storage{
+		URL: "",
+	},
+	Backup: Backup{
+		Limits: BackupLimits{MaxCount: 0},
+	},
+}
 
 func Load() Config {
 	onceLoader.Do(func() {
 		godotenv.Load()
-		envconfig.Process("", &config)
+		envconfig.MustProcess("", &config)
 
 		flag.StringVar(&config.MariaDB.User, "db-user", config.MariaDB.User, "Database User")
 		flag.StringVar(&config.MariaDB.Password, "db-password", config.MariaDB.Password, "Database Password")
