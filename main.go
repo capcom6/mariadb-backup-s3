58ac897801f6d542dd4921d92b06ee490e9cb44f
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-10-10 07:00:24 +0700

    [commands] draft restore command

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/capcom6/mariadb-backup-s3/internal/cli/commands/backup"
+	"github.com/capcom6/mariadb-backup-s3/internal/cli/commands/restore"
 	"github.com/capcom6/mariadb-backup-s3/internal/core/codes"
 	"github.com/joho/godotenv"
 	"github.com/urfave/cli/v3"
@@ -24,6 +25,7 @@ func main() {
 		DefaultCommand: "backup",
 		Commands: []*cli.Command{
 			backup.Command(),
+			restore.Command(),
 		},
 		Flags: []cli.Flag{},
 		Authors: []any{
